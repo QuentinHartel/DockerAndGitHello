cf29173fcd00ce254b0409b60d81e98e9561f843
QuentinHartel <112497036+QuentinHartel@users.noreply.github.com>
2023-10-20 09:12:12 -0600

    Cosmic path and make file

---
+++ cosmicpathoptimization/cosmic_path.py
@@ -0,0 +1,13 @@
+def calculate_mean(temp_list: list[int], num_temps: int) -> int:
+    return(int(sum(temp_list)/num_temps))
+
+
+def main():
+    num_temps = int(input())
+    str_temp_list = input().split()
+    temp_list = [int(temp) for temp in str_temp_list]
+    print(calculate_mean(temp_list, num_temps))
+
+
+if __name__ == "__main__":
+    main()
