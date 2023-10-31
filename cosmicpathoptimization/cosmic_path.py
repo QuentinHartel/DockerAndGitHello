d8b793d8a28622b769cf3b66569297a4104f7b80
QuentinHartel <112497036+QuentinHartel@users.noreply.github.com>
2023-10-30 21:14:27 -0600

    mypy fix

--- cosmicpathoptimization/cosmic_path.py
+++ cosmicpathoptimization/cosmic_path.py
@@ -7,7 +7,7 @@ def calculate_mean(temp_list: intList, num_temps: int) -> int:
     return (int(sum(temp_list) / num_temps))
 
 
-def main():
+def main() -> None:
     num_temps = int(input())
     str_temp_list = input().split()
     temp_list = [int(temp) for temp in str_temp_list]
